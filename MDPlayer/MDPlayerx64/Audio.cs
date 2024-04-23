3f929e7e2b45f7440c99ca607c2da6ea5b1ede5e
kumatan <kumatan@kumymelody.jp>
2024-04-23 18:57:04 +0900

    STBL378 2024/04/23 2nd   Info:mp3の曲情報を表示できるようにした

--- MDPlayer/MDPlayerx64/Audio.cs
+++ MDPlayer/MDPlayerx64/Audio.cs
@@ -792,201 +792,31 @@ namespace MDPlayer
             }
             else if (ext == ".mp3")
             {
-                //さんこう ：
-                //https://so-zou.jp/software/tech/programming/c-sharp/media/audio/naudio/#no2
-
-                try
+                music.format = EnmFileFormat.MP3;
+                GD3 gd3 = new mp3().getGD3Info(buf, 0);
+                if (gd3 != null)
                 {
-                    MemoryStream mStream = new MemoryStream(buf);
-                    Mp3FileReader r = new Mp3FileReader(mStream);
-                    byte[] v1 = r.Id3v1Tag;
-                    Encoding enc = Encoding.Default;
-                    string v1Title = null;
-                    string v1Artist = null;
-                    string v1Album = null;
-                    string v1Year = null;
-                    if (v1 != null)
-                    {
-                        char[] trimChars = { '\0' };
-                        v1Title = enc.GetString(v1, 3, 30).TrimEnd(trimChars);
-                        v1Artist = enc.GetString(v1, 33, 30).TrimEnd(trimChars);
-                        v1Album = enc.GetString(v1, 63, 30).TrimEnd(trimChars);
-                        v1Year = enc.GetString(v1, 93, 4).TrimEnd(trimChars);
-                    }
+                    music.title = gd3.TrackName;
+                    music.titleJ = gd3.TrackNameJ;
+                    music.game = gd3.GameName;
+                    music.gameJ = gd3.GameNameJ;
+                    music.composer = gd3.Composer;
+                    music.composerJ = gd3.ComposerJ;
+                    music.vgmby = gd3.VGMBy;
 
-                    byte[] v2 = r.Id3v2Tag.RawData;
-
-                    if (v2[0] != 0x49 || v2[1] != 0x44 || v2[2] != 0x33)
-                    {
-                        throw new Exception("Error ID3v2TAG Header.");
-                    }
-
-                    int v = v2[3];
-                    int flag = v2[5];
-                    uint size = (uint)((v2[6] << 21) + (v2[7] << 14) + (v2[8] << 7) + (v2[9] << 0));
-
-                    Dictionary<string, string> tags = new Dictionary<string, string>();
-
-
-                    if (v == 2)
-                    {
-                        for (int index = 10; index < v2.Length;)
-                        {
-                            if (v2[index] == '\0') break;
-                            string frameID = Encoding.ASCII.GetString(v2, index, 3);
-                            index += 3;
-                            int frameSize = (int)((v2[index] << 16) + (v2[index + 1] << 8) + v2[index + 2]);
-                            index += 3;
-
-                            enc = Encoding.Default;// Unicode; // UTF-16LE
-                            byte c = v2[index++];
-                            switch (c)
-                            {
-                                case 0x00:
-                                    enc = Encoding.GetEncoding(28591); // iso-8859-1
-                                    break;
-
-                                case 0x01:
-                                    enc = Encoding.GetEncoding("UTF-16");//.BigEndianUnicode; // UTF-16BE
-                                    break;
-                            }
-
-                            string content = enc.GetString(v2, index, frameSize -1);
-                            index += frameSize - 1;
-
-                            if (!tags.ContainsKey(frameID)) tags.Add(frameID, content);
-                        }
-
-                        music.title = string.Format("({0})", System.IO.Path.GetFileName(file));
-                        if (tags.ContainsKey("TT2"))
-                        {
-                            string title = tags["TT2"];
-                            music.title = string.IsNullOrEmpty(title)
-                                ? (
-                                    string.IsNullOrEmpty(v1Title)
-                                    ? string.Format("({0})", System.IO.Path.GetFileName(file)) : v1Title
-                                    )
-                                : title;
-                            music.titleJ = music.title;
-                        }
-
-                        if (tags.ContainsKey("TP1"))
-                        {
-                            string artist = tags["TP1"];
-                            music.composer = string.IsNullOrEmpty(artist)
-                                ? (
-                                    string.IsNullOrEmpty(v1Artist)
-                                    ? "" : v1Artist
-                                    )
-                                : artist;
-                            music.composerJ = music.composer;
-                        }
-
-                        if (tags.ContainsKey("TAL"))
-                        {
-                            string album = tags["TAL"];
-                            music.game = string.IsNullOrEmpty(album)
-                                ? (
-                                    string.IsNullOrEmpty(v1Album)
-                                    ? "" : v1Album
-                                    )
-                                : album;
-                            music.gameJ = music.game;
-                        }
-                    }
-                    else
-                    {
-                        for (int index = 10; index < v2.Length;)
-                        {
-                            if (v2[index] == '\0') break;
-
-                            string frameID = Encoding.ASCII.GetString(v2, index, 4);
-                            index += 4;
-
-                            if (BitConverter.IsLittleEndian)
-                            {
-                                Array.Reverse(v2, index, 4);
-                            }
-                            int frameSize = BitConverter.ToInt32(v2, index);
-                            index += 4;
-
-                            byte flag1 = v2[index++];
-                            byte flag2 = v2[index++];
-
-                            if (frameID[0] == 'T')
-                            {
-                                enc = Encoding.Default;// Unicode; // UTF-16LE
-                                byte c = v2[index++];
-                                switch (c)
-                                {
-                                    case 0x00:
-                                        enc = Encoding.GetEncoding(28591); // iso-8859-1
-                                        break;
-
-                                    case 0x01:
-                                        if (index + 1 < v2.Length && (v2[index] == 0xfe && v2[index + 1] == 0xff))
-                                        {
-                                            enc = Encoding.BigEndianUnicode; // UTF-16BE
-                                        }
-                                        break;
-                                }
-
-                                string content = enc.GetString(v2, index, frameSize - 1);
-                                index += frameSize - 1;
-
-                                if (!tags.ContainsKey(frameID)) tags.Add(frameID, content);
-                            }
-                            else
-                            {
-                                index += frameSize;
-                            }
-                        }
-
-                        music.title = string.Format("({0})", System.IO.Path.GetFileName(file));
-                        if (tags.ContainsKey("TIT2"))
-                        {
-                            string title = tags["TIT2"];
-                            music.title = string.IsNullOrEmpty(title)
-                                ? (
-                                    string.IsNullOrEmpty(v1Title)
-                                    ? string.Format("({0})", System.IO.Path.GetFileName(file)) : v1Title
-                                    )
-                                : title;
-                            music.titleJ = music.title;
-                        }
-
-                        if (tags.ContainsKey("TPE1"))
-                        {
-                            string artist = tags["TPE1"];
-                            music.composer = string.IsNullOrEmpty(artist)
-                                ? (
-                                    string.IsNullOrEmpty(v1Artist)
-                                    ? "" : v1Artist
-                                    )
-                                : artist;
-                            music.composerJ = music.composer;
-                        }
-
-                        if (tags.ContainsKey("TALB"))
-                        {
-                            string album = tags["TALB"];
-                            music.game = string.IsNullOrEmpty(album)
-                                ? (
-                                    string.IsNullOrEmpty(v1Album)
-                                    ? "" : v1Album
-                                    )
-                                : album;
-                            music.gameJ = music.game;
-                        }
-                    }
-
+                    music.converted = gd3.Converted;
+                    music.notes = gd3.Notes;
                 }
-                catch
+                else
                 {
                     music.title = string.Format("({0})", System.IO.Path.GetFileName(file));
                 }
 
-                music.format = EnmFileFormat.MP3;
+                if (music.title == "" && music.titleJ == "")
+                {
+                    music.title = string.Format("({0})", System.IO.Path.GetFileName(file));
+                }
+
             }
             else if (ext == ".aiff")
             {
@@ -2519,6 +2349,13 @@ namespace MDPlayer
 
             if (PlayingFileFormat == EnmFileFormat.MP3)
             {
+                try
+                {
+                    wavestreamGD3 = (new mp3()).getGD3Info(File.ReadAllBytes(naudioFileName), 0);
+                }
+                catch {
+                    wavestreamGD3 = null;
+                }
                 naudioMp3FileReader = new Mp3FileReader(naudioFileName);
                 WaveFormat format = new WaveFormat(setting.outputDevice.SampleRate, 16, 2);
                 wfcp = new WaveFormatConversionProvider(format, naudioMp3FileReader);
@@ -8616,9 +8453,11 @@ namespace MDPlayer
                 {
                     mds.setIncFlag();
                     DriverVirtual.vstDelta = 0;
+
                     for (i = 0; i < sampleCount; i += 2)
                     {
                         cnt = (Int32)mXDRV.Render(buffer, offset + i, 2);
+
                         mds.Update(buffer, offset + i, 2, null);
                     }
                     //cnt = (Int32)((Driver.MXDRV.MXDRV)driverVirtual).Render(buffer, offset , sampleCount);
@@ -8733,6 +8572,7 @@ namespace MDPlayer
         private static byte[] naudioSrcbuffer = null;
         private static long naudioSampleCounter = 0;
         private static int naudioDummyCount = 0;
+        private static GD3 wavestreamGD3;
 
         public static void Seek(double n)
         {
@@ -9013,8 +8853,17 @@ namespace MDPlayer
 
         public static GD3 GetGD3()
         {
-            if (DriverVirtual != null) return DriverVirtual.GD3;
-            return null;
+            if (DriverVirtual == null)
+            {
+                WaveStream ws = null;
+                if (naudioMp3FileReader != null){
+                    ws = naudioMp3FileReader;
+                    return wavestreamGD3;
+                }
+
+                return null;
+            }
+            else return DriverVirtual.GD3;
         }
 
 
---
+++ MDPlayer/MDPlayerx64/Driver/mp3.cs
@@ -0,0 +1,233 @@
+﻿using MDPlayer;
+using MDPlayer.Driver.MuSICA;
+using NAudio.Midi;
+using NAudio.Wave;
+using NAudio.Wave.Compression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MDPlayer.PlayList;
+using static MDPlayer.xgm;
+using static MDSound.XGMFunction;
+
+namespace MDPlayerx64.Driver
+{
+    public class mp3 : baseDriver
+    {
+        public override GD3 getGD3Info(byte[] buf, uint vgmGd3)
+        {
+            GD3 ret = new GD3();
+
+            try
+            {
+                //さんこう ：
+                //https://so-zou.jp/software/tech/programming/c-sharp/media/audio/naudio/#no2
+
+                MemoryStream mStream = new MemoryStream(buf);
+                Mp3FileReader r = new Mp3FileReader(mStream);
+                byte[] v1 = r.Id3v1Tag;
+                Encoding enc = Encoding.Default;
+                string v1Title = null;
+                string v1Artist = null;
+                string v1Album = null;
+                string v1Year = null;
+                if (v1 != null)
+                {
+                    char[] trimChars = { '\0' };
+                    v1Title = enc.GetString(v1, 3, 30).TrimEnd(trimChars);
+                    v1Artist = enc.GetString(v1, 33, 30).TrimEnd(trimChars);
+                    v1Album = enc.GetString(v1, 63, 30).TrimEnd(trimChars);
+                    v1Year = enc.GetString(v1, 93, 4).TrimEnd(trimChars);
+                }
+
+                byte[] v2 = r.Id3v2Tag.RawData;
+
+                if (v2[0] != 0x49 || v2[1] != 0x44 || v2[2] != 0x33)
+                {
+                    throw new Exception("Error ID3v2TAG Header.");
+                }
+
+                int v = v2[3];
+                int flag = v2[5];
+                uint size = (uint)((v2[6] << 21) + (v2[7] << 14) + (v2[8] << 7) + (v2[9] << 0));
+
+                Dictionary<string, string> tags = new Dictionary<string, string>();
+
+
+                if (v == 2)
+                {
+                    for (int index = 10; index < v2.Length;)
+                    {
+                        if (v2[index] == '\0') break;
+                        string frameID = Encoding.ASCII.GetString(v2, index, 3);
+                        index += 3;
+                        int frameSize = (int)((v2[index] << 16) + (v2[index + 1] << 8) + v2[index + 2]);
+                        index += 3;
+
+                        enc = Encoding.Default;// Unicode; // UTF-16LE
+                        byte c = v2[index++];
+                        switch (c)
+                        {
+                            case 0x00:
+                                enc = Encoding.GetEncoding(28591); // iso-8859-1
+                                break;
+
+                            case 0x01:
+                                enc = Encoding.GetEncoding("UTF-16");//.BigEndianUnicode; // UTF-16BE
+                                break;
+                        }
+
+                        string content = enc.GetString(v2, index, frameSize - 1);
+                        index += frameSize - 1;
+
+                        if (!tags.ContainsKey(frameID)) tags.Add(frameID, content);
+                    }
+
+                    if (tags.ContainsKey("TT2"))
+                    {
+                        string title = tags["TT2"];
+                        GD3.TrackName = string.IsNullOrEmpty(title)
+                            ? (
+                                string.IsNullOrEmpty(v1Title)
+                                ? "" : v1Title
+                        )
+                            : title;
+                        GD3.TrackNameJ = GD3.TrackName;
+                    }
+
+                    if (tags.ContainsKey("TP1"))
+                    {
+                        string artist = tags["TP1"];
+                        GD3.Composer = string.IsNullOrEmpty(artist)
+                            ? (
+                                string.IsNullOrEmpty(v1Artist)
+                                ? "" : v1Artist
+                        )
+                            : artist;
+                        GD3.ComposerJ = GD3.Composer;
+                    }
+
+                    if (tags.ContainsKey("TAL"))
+                    {
+                        string album = tags["TAL"];
+                        GD3.GameName = string.IsNullOrEmpty(album)
+                            ? (
+                                string.IsNullOrEmpty(v1Album)
+                                ? "" : v1Album
+                                )
+                            : album;
+                        GD3.GameNameJ = GD3.GameName;
+                    }
+                }
+                else
+                {
+                    for (int index = 10; index < v2.Length;)
+                    {
+                        if (v2[index] == '\0') break;
+
+                        string frameID = Encoding.ASCII.GetString(v2, index, 4);
+                        index += 4;
+
+                        if (BitConverter.IsLittleEndian)
+                        {
+                            Array.Reverse(v2, index, 4);
+                        }
+                        int frameSize = BitConverter.ToInt32(v2, index);
+                        index += 4;
+
+                        byte flag1 = v2[index++];
+                        byte flag2 = v2[index++];
+
+                        if (frameID[0] == 'T')
+                        {
+                            enc = Encoding.Default;// Unicode; // UTF-16LE
+                            byte c = v2[index++];
+                            switch (c)
+                            {
+                                case 0x00:
+                                    enc = Encoding.GetEncoding(28591); // iso-8859-1
+                                    break;
+
+                                case 0x01:
+                                    if (index + 1 < v2.Length && (v2[index] == 0xfe && v2[index + 1] == 0xff))
+                                    {
+                                        enc = Encoding.BigEndianUnicode; // UTF-16BE
+                                    }
+                                    break;
+                            }
+
+                            string content = enc.GetString(v2, index, frameSize - 1);
+                            index += frameSize - 1;
+
+                            if (!tags.ContainsKey(frameID)) tags.Add(frameID, content);
+                        }
+                        else
+                        {
+                            index += frameSize;
+                        }
+                    }
+
+                    if (tags.ContainsKey("TIT2"))
+                    {
+                        string title = tags["TIT2"];
+                        GD3.TrackName = string.IsNullOrEmpty(title)
+                            ? (
+                                string.IsNullOrEmpty(v1Title)
+                                ? "" : v1Title
+                        )
+                            : title;
+                        GD3.TrackNameJ = GD3.TrackName;
+                    }
+
+                    if (tags.ContainsKey("TPE1"))
+                    {
+                        string artist = tags["TPE1"];
+                        GD3.Composer = string.IsNullOrEmpty(artist)
+                            ? (
+                                string.IsNullOrEmpty(v1Artist)
+                                ? "" : v1Artist
+                        )
+                            : artist;
+                        GD3.ComposerJ = GD3.Composer;
+                    }
+
+                    if (tags.ContainsKey("TALB"))
+                    {
+                        string album = tags["TALB"];
+                        GD3.GameName = string.IsNullOrEmpty(album)
+                            ? (
+                                string.IsNullOrEmpty(v1Album)
+                                ? "" : v1Album
+                                )
+                            : album;
+                        GD3.GameNameJ = GD3.GameName;
+                    }
+                }
+
+            }
+            catch
+            {
+                GD3.TrackName = "";
+            }
+
+            return GD3;
+        }
+
+        public override bool init(byte[] vgmBuf, ChipRegister chipRegister, EnmModel model, EnmChip[] useChip, uint latency, uint waitTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool init(byte[] vgmBuf, int fileType, ChipRegister chipRegister, EnmModel model, EnmChip[] useChip, uint latency, uint waitTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void oneFrameProc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
